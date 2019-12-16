1277a9902105c4e33c930a83e1c239069b18941c
amitmichael <amitmichael22@gmail.com>
2019-12-16 22:32:13 +0200

    2

--- src/main/java/bgu/spl/mics/AgentsAvailableEvent.java
+++ src/main/java/bgu/spl/mics/AgentsAvailableEvent.java
@@ -1,4 +1,26 @@
 package bgu.spl.mics;
 
+import java.util.List;
+
 public class AgentsAvailableEvent implements Event<String> {
+    private String msg;
+    private Future fut;
+    private List<String> serials;
+
+    public AgentsAvailableEvent(String msg,List<String> serials)
+    {
+        this.msg = msg;
+        this.serials=serials;
+    }
+
+    public void setFut(Future fut){
+        this.fut=fut;
+    }
+
+    public Future getFut(){
+        return fut;
+    }
+    public List<String> getserials(){
+        return serials;
+    }
 }
